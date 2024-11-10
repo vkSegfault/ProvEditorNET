42ec5adb7353d0eee1664a9aa96510818777503e
vkSegfault <adtofaust@gmail.com>
2024-11-10 21:52:51 +0100

    upd

--- Repository/UserDbContext.cs
+++ Repository/UserDbContext.cs
@@ -1,11 +1,14 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ProvEditorNET.Repository;
 
-public class UserDbContext: IdentityDbContext
+// public class UserDbContext: IdentityDbContext<IdentityUser>
+public class UserDbContext: IdentityDbContext<IdentityUser>
 {
-    public UserDbContext(DbContextOptions options) : base(options)
+    // public UserDbContext(DbContextOptions options) : base(options)
+    public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
     {
         
     }
