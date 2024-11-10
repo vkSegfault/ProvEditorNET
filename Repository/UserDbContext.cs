2148b1f218c0bb0957046d8c173dd4874bb5cd71
vkSegfault <adtofaust@gmail.com>
2024-11-10 17:48:57 +0100

    add dbcontext for users login

--- Repository/UserDbContext.cs
+++ Repository/UserDbContext.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProvEditorNET.Repository;
 
 public class UserDbContext: IdentityDbContext
 {
-    
+    public UserDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
 }
--- obj/Debug/net8.0/ProvEditorNET.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProvEditorNET.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProvEditorNET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14f9465327b9e45081bf30d6ff4c5d966cd0c109")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProvEditorNET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProvEditorNET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
