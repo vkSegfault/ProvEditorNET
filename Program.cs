6bec1f708f7c552d42d8757262a1cf86e7b8319d
vkSegfault <adtofaust@gmail.com>
2024-11-11 18:06:43 +0100

    Add Google SSO initial setup

--- Program.cs
+++ Program.cs
@@ -36,7 +36,11 @@ builder.Services.AddAuthentication().AddCookie(IdentityConstants.ApplicationSche
 builder.Services.AddIdentityCore<User>().AddEntityFrameworkStores<IdentityDbContext>().AddApiEndpoints();
 
 // Google SSO
-
+builder.Services.AddAuthentication().AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = "592195124025-6g06a3tddd3fpu494rsrplopn83f7jb2.apps.googleusercontent.com";
+    googleOptions.ClientSecret = "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";
+});
 
 var app = builder.Build();
 
