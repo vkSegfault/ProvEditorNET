6c395227887ba52b4c0012f28576e70cbd04f113
vkSegfault <adtofaust@gmail.com>
2024-11-13 17:17:03 +0100

    revert back to simple authorize with base Identity

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
@@ -38,25 +40,31 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.Lockout.AllowedForNewUsers = true;
     options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
     options.Lockout.MaxFailedAccessAttempts = 5;
-    
+
     options.User.RequireUniqueEmail = true;
 });
 
 // use simple authorization directly from Indentity package or cutomized authenitaction (needed for SSO)
-// builder.Services.AddAuthorization();
-// builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<IdentityDbContext>();
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<IdentityDbContext>();
 
 // alternative when using our custom defined IdentityUser
-builder.Services.AddAuthentication().AddCookie(IdentityConstants.ApplicationScheme).AddBearerToken(IdentityConstants.BearerScheme);
-builder.Services.AddIdentityCore<User>().AddEntityFrameworkStores<IdentityDbContext>().AddApiEndpoints();
+// builder.Services.AddAuthentication(options =>
+//     {
+//         options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+//         options.DefaultChallengeScheme = BearerTokenDefaults.AuthenticationScheme;
+//     })
+//     .AddCookie(IdentityConstants.ApplicationScheme)
+//     .AddBearerToken(IdentityConstants.BearerScheme);
+// builder.Services.AddIdentityCore<User>().AddEntityFrameworkStores<IdentityDbContext>().AddApiEndpoints();
 
 // Google SSO
-builder.Services.AddAuthentication().AddGoogle(googleOptions =>
-{
-    googleOptions.SignInScheme = IdentityConstants.ExternalScheme;
-    googleOptions.ClientId = "592195124025-6g06a3tddd3fpu494rsrplopn83f7jb2.apps.googleusercontent.com";
-    googleOptions.ClientSecret = "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";
-});
+// builder.Services.AddAuthentication().AddGoogle(googleOptions =>
+// {
+//     googleOptions.SignInScheme = IdentityConstants.ExternalScheme;
+//     googleOptions.ClientId = "592195124025-6g06a3tddd3fpu494rsrplopn83f7jb2.apps.googleusercontent.com";
+//     googleOptions.ClientSecret = "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";
+// });
 
 var app = builder.Build();
 
@@ -68,9 +76,8 @@ if (app.Environment.IsDevelopment())
     // app.ApplyMigrations();   // TODO - what package ?
 }
 
-// app.MapIdentityApi<IdentityUser>();   // used for crude Authorization directly from Identity package (without any custom changes)
-app.MapIdentityApi<User>();
-app.UseHttpsRedirection();
+app.MapIdentityApi<IdentityUser>();   // used for crude Authorization directly from Identity package (without any custom changes)
+// app.MapIdentityApi<User>();
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
--- Repository/IdentityDbContext.cs
+++ Repository/IdentityDbContext.cs
@@ -6,8 +6,8 @@ using ProvEditorNET.Models;
 namespace ProvEditorNET.Repository;
 
 // IdentityUser is .NET Core predefined user, but we can add our custom one (with additional fields) by inheriting from IdentityUser and provide it here
-// public class IdentityDbContext: IdentityDbContext<IdentityUser>
-public class IdentityDbContext: IdentityDbContext<User>
+public class IdentityDbContext: IdentityDbContext<IdentityUser>
+// public class IdentityDbContext: IdentityDbContext<User>
 {
     // public UserDbContext(DbContextOptions options) : base(options)
     public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base(options)
