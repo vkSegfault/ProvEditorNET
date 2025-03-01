2686ab1b03181496e439604d4af23b5ab33a644b
vkSegfault <adtofaust@gmail.com>
2025-03-01 11:25:40 +0100

    add validation rule for Page number

--- Validators/GetAllProvinceOptionsDtoValidator.cs
+++ Validators/GetAllProvinceOptionsDtoValidator.cs
@@ -7,6 +7,7 @@ public class GetAllProvinceOptionsDtoValidator : AbstractValidator<GetAllProvinc
 {
     public GetAllProvinceOptionsDtoValidator()
     {
+        RuleFor(o => o.page).GreaterThanOrEqualTo(0).WithMessage("Page must be greater than 0");
         RuleFor(o => o.pageSize).LessThanOrEqualTo(25).WithMessage("Page size must be between 0 and 25");
     }
 }
